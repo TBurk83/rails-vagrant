6ba383533fa785bba2dc6169311625986d72456f
Trevor Burkholder <tburk83@gmail.com>
2016-06-06 23:33:59 -0400

    added bower and bootstrap

--- scripts/rootstrap.sh
+++ scripts/rootstrap.sh
@@ -2,8 +2,13 @@
 
 apt-get update
 apt-get upgrade -y
+
 apt-get install git node postgresql-9.3 libpq-dev postgresql-contrib-9.3 curl -y
 
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo npm install -g bower
+
 # PostgreSQL setup
-sudo -u postgres createuser -s vagrant 
+sudo -u postgres createuser -s vagrant
 
